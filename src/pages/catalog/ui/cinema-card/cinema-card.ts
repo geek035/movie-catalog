6cccdd8bd9665b8528ec6c71b4d06e1811aa282a
geek035 <kudryavtsev.savva1@gmail.com>
2025-10-21 16:36:54 +0300

    feat: finished cinema card

--- src/pages/catalog/ui/cinema-card/cinema-card.ts
+++ src/pages/catalog/ui/cinema-card/cinema-card.ts
@@ -2,11 +2,16 @@ import { Component, computed, input } from '@angular/core';
 import { environment } from '@environments';
 import { CinemaModel } from '@server/models';
 import { CardModule } from 'primeng/card';
+import { ChipModule } from 'primeng/chip';
+import { ImageModule } from 'primeng/image';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-cinema-card',
-  imports: [CardModule],
+  imports: [CardModule, ImageModule, ChipModule, TagModule, ScrollPanelModule],
   templateUrl: './cinema-card.html',
+  styleUrl: './cinema-card.css',
 })
 export class CinemaCard {
   public readonly cinema = input.required<CinemaModel>();
